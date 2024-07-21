4c55c8ee020944623412986483f81a15228ea32f
adavidarevalo <davidarevaloc20@gmail.com>
2024-07-20 21:42:03 -0600

    Added a html template

--- apache-install.sh
+++ apache-install.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
 # Actualiza la lista de paquetes
-sudo apt update -y &&
+sudo apt update -y 
 
-# Instala Apache
-sudo apt install -y apache2 &&
+# Instala Apache, wget y unzip
+sudo apt install -y apache2 wget unzip
 
 # Inicia el servicio Apache
-sudo systemctl start apache2 &&
+sudo systemctl start apache2 
 
 # Habilita Apache para que se inicie en el arranque
-sudo systemctl enable apache2 &&
+sudo systemctl enable apache2 
 
-# Crea una página HTML con "Hola Mundo" y limita permisos
-echo "Hola Mundo" | sudo tee /var/www/html/index.html > /dev/null &&
-sudo chmod 644 /var/www/html/index.html
+# Cambia al directorio /tmp
+cd /tmp
+
+# Descarga el archivo ZIP del sitio web
+wget https://www.free-css.com/assets/files/free-css-templates/download/page296/neogym.zip
+
+# Descomprime el archivo ZIP
+unzip neogym.zip
+
+# Elimina el archivo ZIP descargado
+sudo rm -rf neogym.zip
+
+# Borra el contenido del directorio HTML de Apache
+sudo rm -rf /var/www/html/*
+
+# Copia el contenido descomprimido al directorio HTML de Apache
+sudo cp -r neogym-html/* /var/www/html/
+
+# Elimina el directorio temporal descomprimido
+sudo rm -rf /tmp/neogym-html
+
+# Reinicia el servicio Apache
+sudo systemctl restart apache2
